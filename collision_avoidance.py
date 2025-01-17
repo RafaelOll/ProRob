13f655b675ea302521bc2ee4586a56fb33721f05
Fabian Ganzer <fabianganzer@yahoo.com>
2025-01-17 12:03:13 +0100

    collision avoidance for two drones

--- collision_avoidance.py
+++ collision_avoidance.py
@@ -1,6 +1,7 @@
 import time
 import sys
 import os
+import numpy as np
 
 # Add crazyflie-lib-python to the system path
 sys.path.append(os.path.abspath("crazyflie-lib-python"))
@@ -89,8 +90,8 @@ def start_states_log(scf):
 
 uris = [
     'radio://0/80/2M/8',
-    'radio://0/80/2M/3',
-    'radio://0/80/2M/2'
+    'radio://0/80/2M/2',
+    #'radio://0/80/2M/3'
     # Add more URIs if you want more copters in the swarm
 ]
 
@@ -112,8 +113,70 @@ def run_sequence(scf, sequence):
 u = 0.3
 dt = 3
 
+def check_collision_linear_segment(t_i1, t_f1, r_i1, r_f1, t_i2, t_f2, r_i2, r_f2, epsilon):
+    """checks if two linear trajectories have a collision.
+    returns:
+    - indices at which the collisions occur
+    - times"""
+    t_eval_i = np.max([t_i1, t_i2])
+    t_eval_f = np.min([t_f1, t_f2])
+    t = np.linspace(t_eval_i, t_eval_f, 100)
+    print(f"evaluation for t in [{t_eval_i}, {t_eval_f}]")
+
+    delta = np.zeros(len(t))
+    for k in range(len(t)):
+        v = r_i1-r_i2 + (r_f1-r_i1)*(t[k]-t_i1)/(t_f1-t_i1) - (r_f2-r_i2)*(t[k]-t_i2)/(t_f2-t_i2)
+        delta[k] = np.sqrt(v[0]**2 + v[1]**2)
+
+    indices_collisions = np.where(delta<epsilon)
+    return t[indices_collisions]
+
+
+def position(t, r_i, r_f, t_i, t_f):
+    """returns the position of a linearly displaced object at time t"""
+    return r_i + (r_f-r_i)*(t-t_i)/(t_f-t_i)
+
+
+def construct_sequences(r_i, r_f, T, epsilon):
+    """constructs a pair of sequences that avoid a crash"""
+    t_i0 = 0
+    t_f0 = T
+    t_i1 = 0
+    t_f1 = T
+
+    dx0, dy0 = r_f[0]-r_i[0]
+    sequence0 = [
+        (dx0, dy0, 0, T)
+    ]
+    v0 = np.sqrt(dx0**2+dy0**2)/T
+    t_delay = 2*epsilon/v0
+    print(f"comuted delay time: {t_delay}")
+
+    t_coll = check_collision_linear_segment(t_i0, t_f0, r_i[0], r_f[0], t_i1, t_f1, r_i[1], r_f[1], epsilon)
+
+
+    if len(t_coll) > 0:
+        r_wp = position(t_coll[0], r_i[1], r_f[1], t_i1, t_f1)
+        t_wp = t_coll[0] + t_delay
+
+        dx10, dy10 = r_wp - r_i[1]
+        dx11, dy11 = r_f[1] - r_wp
+        sequence1 = [
+            (dx10, dy10, 0, t_wp),
+            (dx11, dy11, 0, T-t_wp)
+        ]
+    
+    else:
+        dx1, dy1 = r_f[1]-r_i[1]
+        sequence1 = [
+            (dx1, dy1, 0, T)
+        ]
+    
+    return sequence0, sequence1
+        
+
 sequence0 = [
-    (0, u, 0, dt),
+    (0, u, 0, dt),              # dx, dy, dz, dt
     (0, -u, 0, dt)
 ]
 
@@ -126,16 +189,26 @@ sequence2 = [
     (0, -u, 0, dt),
     (0, u, 0, dt)
 ]
-seq_args = {
-    uris[0]: [sequence0],
-    uris[1]: [sequence1],
-    uris[2]: [sequence2]
-}
 
 
 
-if __name__ == '__main__':
+def get_positions():
+    data = np.genfromtxt("cf_log.txt", dtype=None, encoding=None)
     
+    r = np.zeros(shape=(len(uris), 2))
+
+    for i_uri in range(len(uris)):
+        found = False
+        i_data = -1
+        while not found:
+            if data[i_data][1] == uris[i_uri]:
+                found = True
+                r[i_uri] = np.array([data[i_data][2], data[i_data][3]])
+            i_data -= 1           
+    return r
+
+
+def flight():    
     print("init drivers")
     cflib.crtp.init_drivers()
 
@@ -159,7 +232,62 @@ if __name__ == '__main__':
 
             print("Run the test sequence")
             swarm.parallel_safe(take_off)
+            
+            T = 10
+            epsilon = 0.5
+            r_i = get_positions()
+            r_f = np.array(
+                [
+                    [0, 1],
+                    [1, 0],
+                ]
+            )
+
+            seq0, seq1 = construct_sequences(r_i, r_f, T, epsilon)
+            seq_args = {
+                uris[0]: [seq0],
+                uris[1]: [seq1],
+            }
+            print(seq0)
+            print(seq1)
+            time.sleep(10)
+            print("It goes on!")
+
             swarm.parallel_safe(run_sequence, args_dict=seq_args)
             swarm.parallel_safe(land)
 
 
+
+def seq_constr_test():
+    r_i1 = np.array([0, -1])
+    r_f1 = np.array([0, 1])
+    r_i2 = np.array([-1, 0])
+    r_f2 = np.array([1, 0])
+    #t_i1 = 0
+    #t_f1 = 10
+    #t_i2 = 5
+    #t_f2 = 10
+    epsilon = 0.25
+    T = 10
+
+    #t_coll = check_collision_linear_segment(t_i1, t_f1, r_i1, r_f1, t_i2, t_f2, r_i2, r_f2, epsilon)
+    #print(t_coll)
+
+    r_i = np.array([r_i1, r_i2])
+    r_f = np.array([r_f1, r_f2])
+
+    seq0, seq1 = construct_sequences(r_i, r_f, T, epsilon)
+    print(seq0)
+    print(seq1)
+    try:
+        print(seq1[0][0]+seq1[1][0])
+        print(seq1[0][1]+seq1[1][1])
+        print(seq1[0][3]+seq1[1][3])
+    except:
+        print(":)")
+
+
+if __name__ == "__main__":
+    flight()
+
+    
