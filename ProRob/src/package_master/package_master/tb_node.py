9e4477e734e4111295845db499751f1103ad8d1f
ghoti <guichijin@gmail.com>
2025-01-24 09:27:14 +0100

    add turtle control

--- ProRob/src/package_master/package_master/tb_node.py
+++ ProRob/src/package_master/package_master/tb_node.py
@@ -3,6 +3,169 @@ from rclpy.node import Node
 from example_interfaces.srv import SetBool
 from package_master_interfaces.srv import RobotPositions, SendPositions
 
+#added
+class Vector2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def to(self, other):
+        return Vector2(other.x - self.x, other.y - self.y)
+
+    def set(self, x, y):
+        self.x = x
+        self.y = y
+
+    def length(self):
+        return sqrt(self.x*self.x + self.y*self.y)
+        
+class TurtleController(Node):
+
+    def __init__(self, dict_wp, robot_count):
+        super().__init__('turtle_controller')
+        
+        self.publisher_dict = {}
+        self.subscriber_dict = {}
+        
+        for i in range(robot_count):
+            topic_publisher = f"/robot{i}/cmd_vel"
+            topic_subscriber = f'/robot{i}/odom'
+            
+            self.publisher_dict[i] = self.create_publisher(Twist, topic_publisher, 1)
+            
+            self.subscriber_dict[i] = self.create_subscription(
+                Odometry, 
+                topic_subscriber,
+                lambda msg: self.test_robot2(msg, i),
+                1
+            )
+        
+        #self.publisher_ = self.create_publisher(Twist, '/robot1/cmd_vel', 1)
+
+        self.time = 0
+        #self.timer = self.create_timer(1/5, self.move_turtle)
+        self.get_logger().info("Turtle Controller Node has started")
+        
+        self.path = dict_wp
+        self.target_index = 0
+        self.turning = False
+
+#        self.subscription = self.create_subscription(
+#            Odometry,
+#            '/robot1/odom',
+#            self.move_turtle,
+#            1
+#        )
+
+# #added client
+#         self.robot_count = robot_count
+#         self.arrived_robot_pos = {}
+#         self.client_master = self.create_client(RobotPositions, 'get_robot_positions')
+#         while not self.client_master.wait_for_service(timeout_sec=1.0):
+#             self.get_logger().info('service not available, waiting again...')
+# #added
+
+    def test_robot2(self, msg, id):
+        self.get_logger().info(str(id))
+
+    def move_turtle(self, turtle_pose : Odometry, robot_id):
+        ANGLE_THRESHOLD_HIGH = 0.5
+        ANGLE_THRESHOLD_LOW = 0.2
+        k_rot = 1
+        rot_min = 0 # 0.5
+        DST_THRESHOLD = 0.05
+        k_speed = 0.1 # 0.5
+        
+        pose = turtle_pose.pose.pose
+        curr_theta = pose.orientation.z*pi
+        curr_pos = Vector2(pose.position.x, pose.position.y)
+
+        delta = Twist()
+        
+        curr_target = self.path[robot_id][self.target_index]
+        r = self.get_angle(curr_theta, curr_pos, curr_target)
+        signe_r = -1 if r < 0 else 1
+        d = curr_pos.to(curr_target).length()
+        
+        if abs(r) > ANGLE_THRESHOLD_HIGH:
+            self.turning = True
+
+        if time() - self.time > 1/3:
+            self.get_logger().info(f"ID: {robot_id}")
+            self.get_logger().info(f"D:{d:2f}, R:{(r*180/pi):2f}")
+            if self.turning: 
+                self.get_logger().info("action = turning")
+            else:
+                self.get_logger().info("action = forwarding")
+            self.get_logger().info(f"current: (x={curr_pos.x:2f}, y={curr_pos.y:2f}), theta={(curr_theta*180/pi):2f}")
+            self.get_logger().info("----------------------------")
+            
+            self.time = time()
+
+#changed
+        global arrived_robot_pos
+        if d < DST_THRESHOLD:
+            self.get_logger().info(f"arrived wp: {self.target_index}")
+
+            if self.target_index==len(self.path)-1:
+                self.arrived_robot_pos[f"tb{robot_id}"] = (curr_pos.x, curr_pos.y, 0)
+                arrived_robot_pos[f"tb{robot_id}"] = (curr_pos.x, curr_pos.y, 0)
+                
+            else: 
+                self.target_index += 1
+
+    # # added
+    #         if self.target_index==len(self.path)-1:
+    #             self.arrived_robot_pos[f"tb{robot_id}"] = (curr_pos.x, curr_pos.y, 0)
+    #             if len(self.arrived_robot_pos)==self.robot_count:
+    #                 self.send_positions(self.arrived_robot_pos)
+                
+    #         else: 
+    #             self.target_index += 1
+    # # added
+#changed
+
+        elif self.turning:
+            delta.angular.z = max(abs(k_rot*r/pi), rot_min)*signe_r
+            r = self.get_angle(curr_theta, curr_pos, curr_target)
+            if abs(r) < ANGLE_THRESHOLD_LOW:
+                self.turning = False    
+        else:
+            delta.linear.x = k_speed*min(d, 0.4)
+            d = curr_pos.to(curr_target).length() # Update distance to travel left
+        
+            
+        self.publisher_dict[robot_id].publish(delta)
+        
+    
+    def get_angle(self, theta, pos1, pos2):
+        dir = pos1.to(pos2)
+        theta_turn = atan2(dir.y, dir.x) - theta
+        if theta_turn > pi:
+            theta_turn = theta_turn-2*pi
+        elif theta_turn < -pi:
+            theta_turn = theta_turn+2*pi
+        return theta_turn
+
+# #added
+#     def send_positions(self, arrived_robot_pos): # a modifier
+#         request = RobotPositions.Request()
+
+#         request.all_robot_pos = arrived_robot_pos
+
+#         self.get_logger().info(f"Sending position of robots finished")
+
+#         future = self.client_send_positions.call_async(request)
+#         rclpy.spin_until_future_complete(self, future)
+
+#         if future.result():
+#             self.get_logger().info(f"Server Response: {str(future.result())}")
+#         else:
+#             self.get_logger().warn("Failed to send robot position")
+
+# #added
+
+
 
 
 class GetPosService(Node):
@@ -32,8 +195,12 @@ class SendPosService(Node):
 
     #def make_the_move(self): move the turtlebot the send their final location with get_position
 
+
+
     def get_position(self, request):
-        robot_position_map = request.all_robot_pos
+        # robot_position_map = request.all_robot_pos
+        global arrived_robot_pos
+        robot_position_map = arrived_robot_pos
         final_pos_dict = {
             name: robot_position_map.get(name, (0.0, 0.0, 0.0))
             for name in request.robot_names
@@ -67,6 +234,25 @@ def main(args=None):
             break
     print("stage 1 done")
     print(position_dict)
+
+
+    robot_count = 1
+    arrived_robot_pos = {}
+
+    dict_wp = {}
+    dict_wp[0] = [Vector2(2,0.0)]
+    dict_wp[1] = [Vector2(1,-1)]
+    dict_wp[2] = [Vector2(2,0)]
+    
+    turtle_controller = TurtleController(dict_wp, robot_count)
+
+    while rclpy.ok():
+        rclpy.spin_once(turtle_controller)
+        if len(arrived_robot_pos)==robot_count: 
+            turtle_controller.destroy_node()
+            break
+
+    
 
     # Lancer SendPosService
     send_pos_service = SendPosService()
